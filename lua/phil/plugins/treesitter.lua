8967741f56c8958b9e6c2d59919801f276a94ba3
Phil Win <phil-win@hotmail.com>
2024-12-16 20:39:27 +0100

    treesitter.lua

---
+++ lua/phil/plugins/treesitter.lua
@@ -0,0 +1,59 @@
+  -- Highlight, edit, and navigate code
+return {
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag", -- Autotagging plugin dependency
+  },
+  config = function()
+    -- import nvim-treesitter plugin
+    local treesitter = require("nvim-treesitter.configs")
+
+    -- configure treesitter
+    treesitter.setup({ -- enable syntax highlighting
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { 'ruby' }, -- Use regex highlighting for Ruby
+      },
+      indent = { enable = true, disable = { 'ruby' }},  -- Enable indentation, disable for Ruby 
+      autotag = { enable = true }, -- enable autotagging
+
+      -- ensure these language parsers are installed
+      ensure_installed = {
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "yaml",
+        "html",
+        "css",
+        "prisma",
+        "markdown",
+        "markdown_inline",
+        "svelte",
+        "graphql",
+        "bash",
+        "lua",
+        "vim",
+        "dockerfile",
+        "gitignore",
+        "query",
+        "vimdoc",
+        "c",
+      },
+
+      -- Enable incremental selection
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+      auto_install = true, -- Automatically install missing parsers when entering buffer
+    })
+  end,
+}
