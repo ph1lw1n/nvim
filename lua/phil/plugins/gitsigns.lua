f02454374daa8474df7e5f8ce82a969007347c6d
Phil Win <phil-win@hotmail.com>
2024-12-18 16:10:50 +0100

    added gitsigns.lua and lazygit.lua

---
+++ lua/phil/plugins/gitsigns.lua
@@ -0,0 +1,14 @@
+-- Adds git related signs to the gutter
+return {
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    -- See `:help gitsigns.txt`
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+  },
+}
