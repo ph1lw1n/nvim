4e8c4c841f63935bd04d45a39b678bf45a2f7dee
Phil Win <philwin@MacBookPro.fritz.box>
2024-12-21 00:49:47 +0100

    toggleterm

--- lua/phil/plugins/init.lua
+++ lua/phil/plugins/init.lua
@@ -1,14 +1,38 @@
 -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
 return {
-  "nvim-tree/nvim-web-devicons",
-  "RRethy/vim-illuminate", -- highlighting same words
-  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
-  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
-  "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-  "tpope/vim-fugitive", -- Powerful Git integration for Vim
-  "tpope/vim-rhubarb", -- Github integration for vim-fugitive
-  "norcalli/nvim-colorizer.lua", -- High-performance color highlighter
-  config = function()
-    require("colorizer").setup()
-  end,
+  "nvim-tree/nvim-web-devicons",  -- Add devicons for file icons
+
+  "RRethy/vim-illuminate",  -- Highlighting same words
+
+  "nvim-lua/plenary.nvim",  -- Lua functions that many plugins use
+
+  "christoomey/vim-tmux-navigator",   -- Tmux & split window navigation
+
+  "tpope/vim-sleuth",  -- Detect tabstop and shiftwidth automatically
+
+  "tpope/vim-fugitive",  -- Powerful Git integration for Vim
+
+  "tpope/vim-rhubarb",  -- GitHub integration for vim-fugitive
+
+  -- Toggle terminal integration
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {},
+    config = function()
+      require("toggleterm").setup({
+        -- Add any custom setup for toggleterm here
+      })
+      vim.keymap.set("n", "<space>td", ":ToggleTerm dir=%:p:h<CR>", { desc = "Terminal" })
+    end,
+  },
+
+  -- High-performance color highlighter
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup()
+    end,
+  },
 }
+
