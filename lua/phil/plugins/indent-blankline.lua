96147908fb5f81e0f1c97bfb01b8a9cf9b691e36
Phil Win <philwin@MacBookPro.fritz.box>
2024-12-20 21:13:01 +0100

    indent

--- lua/phil/plugins/indent-blankline.lua
+++ lua/phil/plugins/indent-blankline.lua
@@ -1,28 +1,25 @@
 return {
-  'lukas-reineke/indent-blankline.nvim',
+  "lukas-reineke/indent-blankline.nvim",
   event = { "BufReadPre", "BufNewFile" },
-  main = 'ibl',
+  main = "ibl",
   opts = {
     indent = {
+      char = "▏",
+      -- char = "│",
       -- char = '┊',  -- choose a style
-      -- char = '▏',
       -- char = '⋮',
-      char = '│',
     },
     scope = {
       show_start = false,
       show_end = false,
-      show_exact_scope = false,
     },
     exclude = {
       filetypes = {
-        'help',
-        'startify',
-        'dashboard',
-        'packer',
-        'neogitstatus',
-        'NvimTree',
-        'Trouble',
+        "help",
+        "startify",
+        "dashboard",
+        "NvimTree",
+        "Trouble",
       },
     },
   },
