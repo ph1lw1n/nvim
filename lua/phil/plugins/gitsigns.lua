7b91b7e2526c435c8fc931a09b446fb7cc05f10f
Phil Win <phil-win@hotmail.com>
2024-12-20 15:36:54 +0100

    updated stylelua

--- lua/phil/plugins/gitsigns.lua
+++ lua/phil/plugins/gitsigns.lua
@@ -1,14 +1,14 @@
 -- Adds git related signs to the gutter
 return {
-  'lewis6991/gitsigns.nvim',
+  "lewis6991/gitsigns.nvim",
   opts = {
     -- See `:help gitsigns.txt`
     signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
+      add = { text = "+" },
+      change = { text = "~" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
     },
   },
 }
