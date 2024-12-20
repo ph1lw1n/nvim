5cdbe366e00fc1f611d0234702f3b1c89d55d39c
Phil Win <philwin@MacBookPro.fritz.box>
2024-12-20 22:33:22 +0100

    init.lua webicons

--- lua/phil/plugins/init.lua
+++ lua/phil/plugins/init.lua
@@ -1,11 +1,12 @@
 return {
+  "nvim-tree/nvim-web-devicons",
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-fugitive', -- Powerful Git integration for Vim
-  'tpope/vim-rhubarb', -- Github integration for vim-fugitive
-  'norcalli/nvim-colorizer.lua', -- High-performance color highlighter
-  config = function ()
-    require('colorizer').setup()
+  "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+  "tpope/vim-fugitive", -- Powerful Git integration for Vim
+  "tpope/vim-rhubarb", -- Github integration for vim-fugitive
+  "norcalli/nvim-colorizer.lua", -- High-performance color highlighter
+  config = function()
+    require("colorizer").setup()
   end,
 }
