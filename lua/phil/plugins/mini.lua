549d963d5f0abc9abf27e6435bdb6f16cb3e685f
Phil Win <philwin@MacBookPro.fritz.box>
2024-12-22 21:17:29 +0100

    mini.ai added

--- lua/phil/plugins/mini.lua
+++ lua/phil/plugins/mini.lua
@@ -5,6 +5,14 @@ return {
     -- Mini Icons
     require("mini.icons").setup()
 
+    -- Better Around/Inside textobjects
+    --
+    -- Examples:
+    --  - va)  - [V]isually select [A]round [)]paren
+    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+    --  - ci'  - [C]hange [I]nside [']quote
+    require("mini.ai").setup({ n_lines = 500 })
+
     -- Mini Indentscope
     vim.api.nvim_create_autocmd("FileType", {
       pattern = { "dashboard", "alpha", "help", "startify", "NvimTree", "Trouble" },
