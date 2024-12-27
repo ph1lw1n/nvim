8674f7a0d0f3028ba01fd49c83010ae6abb7b994
Phil Win <phil-win@hotmail.com>
2024-12-27 12:15:43 +0100

    added mini.files

--- lua/phil/plugins/mini.lua
+++ lua/phil/plugins/mini.lua
@@ -2,19 +2,27 @@ return {
   "echasnovski/mini.nvim",
   version = false, -- Always use the latest version
   config = function()
+
+    -- ----------------------------------------------------------------------------
+
     -- Mini Icons
     require("mini.icons").setup()
+
+    -- ----------------------------------------------------------------------------
 
     -- Mini Animate
     require("mini.animate").setup()
 
+    -- ----------------------------------------------------------------------------
+
     -- Better Around/Inside textobjects
-    --
+    require("mini.ai").setup({ n_lines = 500 })
     -- Examples:
     --  - va)  - [V]isually select [A]round [)]paren
     --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
     --  - ci'  - [C]hange [I]nside [']quote
-    require("mini.ai").setup({ n_lines = 500 })
+
+    -- ----------------------------------------------------------------------------
 
     -- Mini Indentscope
     vim.api.nvim_create_autocmd("FileType", {
@@ -34,6 +42,8 @@ return {
       },
     })
 
+    -- ----------------------------------------------------------------------------
+
     -- Mini Surround
     require("mini.surround").setup({
       mappings = {
@@ -46,5 +56,67 @@ return {
         update_n_lines = "sn", -- Update `n_lines`
       },
     })
+
+    -- ----------------------------------------------------------------------------
+
+    -- -- Load external modules first
+    -- local mini_files_km = require("config.modules.mini-files-km")
+    -- local mini_files_git = require("config.modules.mini-files-git")
+    --
+    -- -- Mini Files
+    -- require("mini.files").setup({
+    --   -- No need to copy this inside `setup()`. Will be used automatically.
+    --   {
+    --     -- Customization of shown content
+    --     content = {
+    --       -- Predicate for which file system entries to show
+    --       filter = nil,
+    --       -- What prefix to show to the left of file system entry
+    --       prefix = nil,
+    --       -- In which order to show file system entries
+    --       sort = nil,
+    --     },
+    --
+    --     -- Module mappings created only inside explorer.
+    --     -- Use `''` (empty string) to not create one.
+    --     mappings = {
+    --       close = "q",
+    --       go_in = "l",
+    --       go_in_plus = "L",
+    --       go_out = "h",
+    --       go_out_plus = "H",
+    --       mark_goto = "'",
+    --       mark_set = "m",
+    --       reset = "<BS>",
+    --       reveal_cwd = "@",
+    --       show_help = "g?",
+    --       synchronize = "=",
+    --       trim_left = "<",
+    --       trim_right = ">",
+    --     },
+    --
+    --     -- General options
+    --     options = {
+    --       -- Whether to delete permanently or move into module-specific trash
+    --       permanent_delete = false,
+    --       -- Whether to use for editing directories
+    --       use_as_default_explorer = true,
+    --     },
+    --
+    --     -- Customization of explorer windows
+    --     windows = {
+    --       -- Maximum number of windows to show side by side
+    --       max_number = math.huge,
+    --       -- Whether to show preview of file/directory under cursor
+    --       preview = true,
+    --       -- Width of focused window
+    --       width_focus = 50,
+    --       -- Width of non-focused window
+    --       width_nofocus = 15,
+    --       -- Width of preview window
+    --       width_preview = 25,
+    --     },
+    --   },
+    -- })
   end,
 }
