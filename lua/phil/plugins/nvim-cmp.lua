2bf183e700a59036e69894203824d9d80addf557
Phil Win <philwin@MacBookPro.fritz.box>
2024-12-30 22:35:50 +0100

    copilot.lua + commented out tab / s-tab for autocompletion

--- lua/phil/plugins/nvim-cmp.lua
+++ lua/phil/plugins/nvim-cmp.lua
@@ -25,48 +25,48 @@ return {
       completion = {
         completeopt = "menu,menuone,preview,noselect",
       },
-      snippet = { -- Configure how nvim-cmp interacts with snippet engine
+      snippet = { -- configure how nvim-cmp interacts with snippet engine
         expand = function(args)
           luasnip.lsp_expand(args.body)
         end,
       },
       mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- Previous suggestion
-        ["<C-j>"] = cmp.mapping.select_next_item(), -- Next suggestion
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4), -- Scroll up in documentation
-        ["<C-f>"] = cmp.mapping.scroll_docs(4), -- Scroll down in documentation
-        ["<C-Space>"] = cmp.mapping.complete(), -- Show completion suggestions
-        ["<C-e>"] = cmp.mapping.abort(), -- Close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Confirm selection
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
+        ["<c-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+        ["<c-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+        ["<c-b>"] = cmp.mapping.scroll_docs(-4), -- scroll up in documentation
+        ["<c-f>"] = cmp.mapping.scroll_docs(4), -- scroll down in documentation
+        ["<c-space>"] = cmp.mapping.complete(), -- show completion suggestions
+        ["<c-e>"] = cmp.mapping.abort(), -- close completion window
+        ["<cr>"] = cmp.mapping.confirm({ select = false }), -- confirm selection
+        -- ["<tab>"] = cmp.mapping(function(fallback)
+        --   if cmp.visible() then
+        --     cmp.select_next_item()
+        --   elseif luasnip.expand_or_locally_jumpable() then
+        --     luasnip.expand_or_jump()
+        --   else
+        --     fallback()
+        --   end
+        -- end, { "i", "s" }),
+        -- ["<s-tab>"] = cmp.mapping(function(fallback)
+        --   if cmp.visible() then
+        --     cmp.select_prev_item()
+        --   elseif luasnip.locally_jumpable(-1) then
+        --     luasnip.jump(-1)
+        --   else
+        --     fallback()
+        --   end
+        -- end, { "i", "s" }),
       }),
       sources = cmp.config.sources({
-        { name = "nvim_lsp" }, -- Language Server Protocol
-        { name = "luasnip" }, -- Snippets
-        { name = "buffer" }, -- Text within the current buffer
-        { name = "path" }, -- File system paths
+        { name = "nvim_lsp" }, -- language server protocol
+        { name = "luasnip" }, -- snippets
+        { name = "buffer" }, -- text within the current buffer
+        { name = "path" }, -- file system paths
       }),
       formatting = {
         format = lspkind.cmp_format({
           maxwidth = 50,
-          ellipsis_char = "...", -- Ellipsis for truncated text
+          ellipsis_char = "...", -- ellipsis for truncated text
         }),
       },
     })
