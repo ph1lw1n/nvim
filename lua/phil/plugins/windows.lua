025abea342548f4ef6acfeae8455da7ba7dd362e
Phil Win <phil-win@hotmail.com>
2025-01-02 13:21:00 +0100

    added: windowmaangement

--- lua/phil/plugins/windows.lua
+++ lua/phil/plugins/windows.lua
@@ -1,13 +1,25 @@
+-- Split window management
 return {
-  "anuvyklack/windows.nvim",
-  dependencies = {
-    "anuvyklack/middleclass",
-    "anuvyklack/animation.nvim",
+  {
+    "anuvyklack/windows.nvim",
+    dependencies = {
+      "anuvyklack/middleclass",
+      "anuvyklack/animation.nvim",
+    },
+    config = function()
+      -- Set window-related options
+      vim.o.winwidth = 10
+      vim.o.winminwidth = 10
+      vim.o.equalalways = false
+
+      -- Setup the plugin
+      require("windows").setup()
+    end,
   },
-  -- config = function()
-  --    vim.o.winwidth = 10
-  --    vim.o.winminwidth = 10
-  --    vim.o.equalalways = false
-  --    require('windows').setup()
-  -- end
+},
+{
+  "szw/vim-maximizer",
+  keys = {
+    { "<leader>sm", "<cmd>MaximizerToggle<CR>", desc = "[M]ax/[M]in" },
+  },
 }
