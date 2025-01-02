a653f1155f0f550efeedf21ee74e62c900e91df8
Phil Win <phil-win@hotmail.com>
2025-01-02 15:06:11 +0100

    restructured

--- lua/phil/plugins/debug/trouble.lua
+++ lua/phil/plugins/debug/trouble.lua
@@ -7,7 +7,11 @@ return {
   cmd = "Trouble",
   keys = {
     { "<leader>xw", "<cmd>Trouble diagnostics toggle<CR>", desc = "[W]orkspace diagnostics" },
-    { "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "[D]ocument diagnostics" },
+    {
+      "<leader>xd",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
+      desc = "[D]ocument diagnostics",
+    },
     -- { "<leader>xq", "<cmd>Trouble quickfix toggle<CR>", desc = "Trouble [Q]uickfix list" },
     -- { "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Trouble [L]ocation list" },
     { "<leader>xt", "<cmd>Trouble todo toggle<CR>", desc = "[T]odos" },
