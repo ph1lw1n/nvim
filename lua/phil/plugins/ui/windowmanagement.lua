668e8260ca2e11273b009a4410c6a954fc39ea0f
Phil Win <philwin@MacBookPro.fritz.box>
2025-01-03 21:45:27 +0100

    focustoggle

--- lua/phil/plugins/ui/windowmanagement.lua
+++ lua/phil/plugins/ui/windowmanagement.lua
@@ -7,10 +7,14 @@ return {
         enable = true, -- Enable Focus functionality
         autoresize = {
           enable = true, -- Enable automatic resizing
-          excluded_filetypes = { "NvimTree", "alpha", "dashboard" }, -- Exclude specific filetypes
+          excluded_filetypes = { "NvimTree", "neo-tree", "alpha", "dashboard" }, -- Exclude specific filetypes
           excluded_buftypes = { "nofile", "terminal" }, -- Exclude specific buffer types
         },
       })
+      -- Keybinding for maximizing or equalizing splits
+      -- vim.keymap.set( "n", "<leader>sm", "<cmd>FocusMaximise<CR>", { desc = "[M]ax/[M]in" })
+      -- vim.keymap.set( "n", "<leader>se", "<cmd>FocusEqualise<CR>", { desc = "[E]qual" })
+      vim.keymap.set( "n", "<leader>so", "<cmd>FocusToggle<CR>", { desc = "[O]n/[O]ff" })
     end,
   },
 
